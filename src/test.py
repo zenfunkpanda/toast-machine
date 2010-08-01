5d4ab568cefd03e683b64dfdc9c902fe7ee81213
Panda <panda@pitollo>
2010-08-01 21:13:27 +0200

    test.py now works

--- src/test.py
+++ src/test.py
@@ -1,25 +1,32 @@
 import os
 import select
 import subprocess
-
-import select
 import fcntl
 
-x = subprocess.Popen(["dd","if=/dev/zero","of=/dev/null"], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+def humanSize(file):
+	num = file
+	for x in ['bytes','KB','MB','GB','TB']:
+		if num < 1024.0:
+			return "%3.1f%s" % (num, x)
+		num /= 1024.0
+	return 0
+
+
+x = subprocess.Popen(["dd","if=/home/panda/debian.iso","of=/dev/null"], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 print x.pid
 
 outfile = x.stderr
 outfd = outfile.fileno()
 file_flags = fcntl.fcntl(outfd, fcntl.F_GETFL)
-fcntl.fcntl(outfd, fcntl.F_SETFL, file_flags | os.O_NDELAY)
+fcntl.fcntl(outfd, fcntl.F_SETFL, file_flags | os.O_NOFOLLOW)
 
-while x.poll:
-	ready = select.select([outfd],[],[],5) # wait for input
+while x.poll() == None:
+	ready = select.select([outfd],[],[],.2) # wait for input
 	
 	if len(ready[0]) == 0:
-		print "timeout... continua"
+		os.system("kill -USR1 %s" % x.pid)
 	else:
-		print len(ready[0])
-
-x.kill()
+		tmp = outfile.readline()[:-1]
+		if len(tmp.split()) > 3:
+			print tmp, x.poll()
 	

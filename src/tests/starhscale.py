fec9cbd338fe70400270135ea1f6b6238b8a3195
Panda <panda@Azuz>
2010-08-10 16:06:06 +0200

    commit of some test for new features

---
+++ src/tests/starhscale.py
@@ -0,0 +1,22 @@
+import gtk
+import Widgets
+
+if __name__ == "__main__":
+
+	# register the class as a Gtk widget
+	#gobject.type_register(StarHScale)
+	
+	def stampa(xx,yy,zz):
+		print xx,yy,zz
+
+	win = gtk.Window()
+	win.resize(200,50)
+	win.connect('delete-event', gtk.main_quit)
+	
+	x = Widgets.SingleEdgeSelector("Top")
+	x.connect('clicked', stampa)
+	win.add(x)
+	
+	win.show_all()    
+	gtk.main()
+	
